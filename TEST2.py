1c327eeac83b2944ea5c63de3d4f63b754ef5175
PJSDDL <V15944034768@163.com>
2024-10-03 18:10:29 +0800

    Update TEST2.py

--- TEST2.py
+++ TEST2.py
@@ -1,20 +1,16 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
 
-#产生数据集
-batch_x = []
-N = 30
-def genrate_one_pic:
-    for index_x in range(N):
-        pixel_line = []
-    
-        for index_y in range(N):
-            x = index_x / N
-            y = index_y / N
-        
-            pixel_line.append([x, y, 1])
-
+#载入数据集
+mnist=input_data.read_data_sets('MNIST_data',one_hot=True)
+#设置批次的大小
+batch_size=100
+#计算一共有多少个批次
+n_batch=mnist.train.num_examples//batch_size
 
 #定义初始化权值函数
 def weight_variable(shape):
@@ -26,64 +22,28 @@ def bias_variable(shape):
     initial=tf.constant(0.1,shape=shape)
     return tf.Variable(initial)
 
-#卷积层
-def conv2d(input,filter):
-    return tf.nn.conv2d(input,filter,strides=[1,1,1,1],padding='SAME')
+#图像resize
+def batch_resize(input_img):
+    out_img = np.zeros((batch_size, 256))
+    for index in range(batch_size):
+        reshape_img = np.reshape(input_img[index], (28, 28))
+        resize_img = cv.resize(reshape_img, (16, 16))
+        out_img[index, :] = np.reshape(resize_img, 256)
 
-#池化层
-def max_pool_2x2(value):
-    return tf.nn.max_pool(value,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+    return out_img
 
 #输入层
 #定义两个placeholder
-x=tf.placeholder(tf.float32,[None,784]) #28*28
+x=tf.placeholder(tf.float32,[None,16*16]) #16*16
 y=tf.placeholder(tf.float32,[None,10])
 
-#改变x的格式转为4维的向量[batch,in_hight,in_width,in_channels]
-x_image=tf.reshape(x,[-1,28,28,1])
-
-#卷积、激励、池化操作
-#初始化第一个卷积层的权值和偏置
-W_conv1=weight_variable([5,5,1,32]) #5*5的采样窗口，32个卷积核从1个平面抽取特征
-b_conv1=bias_variable([32]) #每一个卷积核一个偏置值
-
-#把x_image和权值向量进行卷积，再加上偏置值，然后应用于relu激活函数
-h_conv1=tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)
-h_pool1=max_pool_2x2(h_conv1) #进行max_pooling 池化层
-
-#初始化第二个卷积层的权值和偏置
-W_conv2=weight_variable([5,5,32,64]) #5*5的采样窗口，64个卷积核从32个平面抽取特征
-b_conv2=bias_variable([64])
-
-#把第一个池化层结果和权值向量进行卷积，再加上偏置值，然后应用于relu激活函数
-h_conv2=tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2)
-h_pool2=max_pool_2x2(h_conv2) #池化层
-
-#28*28的图片第一次卷积后还是28*28，第一次池化后变为14*14
-#第二次卷积后为14*14，第二次池化后变为了7*7
-#经过上面操作后得到64张7*7的平面
-#全连接层
-#初始化第一个全连接层的权值
-W_fc1=weight_variable([7*7*64,1024])#经过池化层后有7*7*64个神经元，全连接层有1024个神经元
-b_fc1 = bias_variable([1024])#1024个节点
-
-#把池化层2的输出扁平化为1维
-h_pool2_flat = tf.reshape(h_pool2,[-1,7*7*64])
-
-#求第一个全连接层的输出
-h_fc1=tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
-
-#keep_prob用来表示神经元的输出概率
-keep_prob=tf.placeholder(tf.float32)
-h_fc1_drop=tf.nn.dropout(h_fc1,keep_prob)
-
-#初始化第二个全连接层
-W_fc2=weight_variable([1024,10])
-b_fc2=bias_variable([10])
+#全连接
+W = weight_variable([16*16,10])
+b = bias_variable([10])
 
 #输出层
 #计算输出
-prediction=tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
+prediction=tf.nn.softmax(tf.matmul(x,W)+b)
 
 #交叉熵代价函数
 cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction))
@@ -101,12 +61,25 @@ accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 with tf.Session() as sess:
     start_time=time.clock()
     sess.run(tf.global_variables_initializer()) #初始化变量
-    for epoch in range(21): #迭代21次（训练21次）
+    for epoch in range(12): #迭代12次（训练12次）
         for batch in range(n_batch):
             batch_xs,batch_ys=mnist.train.next_batch(batch_size)
-            sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys,keep_prob:0.7}) #进行迭代训练
-        #测试数据计算出准确率
-        acc=sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
+            batch_xs_re = batch_resize(batch_xs)
+            sess.run(train_step,feed_dict={x:batch_xs_re, y:batch_ys}) #进行迭代训练
+        #计算准确率
+        acc=sess.run(accuracy,feed_dict={x:batch_xs_re, y:batch_ys})
         print('Iter'+str(epoch)+',Testing Accuracy='+str(acc))
     end_time=time.clock()
     print('Running time:%s Second'%(end_time-start_time)) #输出运行时间
+
+    np.savetxt('NUM1.txt', batch_xs_re[0], delimiter = ', ', newline = ',\n', header = "NUM1 = {", footer = "};")
+    np.savetxt('NUM2.txt', batch_xs_re[1], delimiter = ', ', newline = ',\n', header = "NUM2 = {", footer = "};")
+    np.savetxt('NUM3.txt', batch_xs_re[2], delimiter = ', ', newline = ',\n', header = "NUM3 = {", footer = "};")
+    np.savetxt('NUM4.txt', batch_xs_re[3], delimiter = ', ', newline = ',\n', header = "NUM4 = {", footer = "};")
+    np.savetxt('NUM5.txt', batch_xs_re[4], delimiter = ', ', newline = ',\n', header = "NUM5 = {", footer = "};")
+    np.savetxt('NUM6.txt', batch_xs_re[5], delimiter = ', ', newline = ',\n', header = "NUM6 = {", footer = "};")
+
+    W = sess.run(W,feed_dict={x:batch_xs_re,y:batch_ys})
+    np.savetxt('W.txt', W, delimiter = ', ', newline = ',\n', header = "W = {", footer = "};")
+    b = sess.run(b,feed_dict={x:batch_xs_re,y:batch_ys})
+    np.savetxt('b.txt', b, delimiter = ', ', newline = ',\n', header = "b = {", footer = "};")
